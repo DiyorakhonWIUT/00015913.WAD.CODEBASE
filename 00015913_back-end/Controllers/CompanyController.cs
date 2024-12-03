5103c41c01496632203e317a7eb21c525c272562
DiyorakhonWIUT <00015913wiut@gmail.com>
2024-12-04 00:44:41 +0500

    Created Update and Delete methods. All controllers are working correctly now.

--- 00015913_back-end/Controllers/CompanyController.cs
+++ 00015913_back-end/Controllers/CompanyController.cs
@@ -23,7 +23,35 @@ namespace _00015913_back_end.Controllers
             return await _context.Companies.ToListAsync();
         }
 
-        
+        // PUT: api/Company/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCompany(int id, Company company)
+        {
+            if (id != company.CompanyId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(company).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CompanyExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
 
         // POST: api/Company
         [HttpPost]
@@ -33,6 +61,27 @@ namespace _00015913_back_end.Controllers
             await _context.SaveChangesAsync();
 
             return CreatedAtAction(nameof(GetCompanies), new { id = company.CompanyId }, company);
+        }
+
+        // DELETE: api/Company/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCompany(int id)
+        {
+            var company = await _context.Companies.FindAsync(id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            _context.Companies.Remove(company);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CompanyExists(int id)
+        {
+            return _context.Companies.Any(e => e.CompanyId == id);
         }
     }
 }
